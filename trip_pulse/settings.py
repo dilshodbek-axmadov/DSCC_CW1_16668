dc183cd6d3e9e63beee8b3f46dc675695be5a6aa
dilshod <wiut00016668@gmail.com>
2026-03-02 17:47:02 +0500

    Update docker-compose file. Domen added

--- trip_pulse/settings.py
+++ trip_pulse/settings.py
@@ -86,9 +86,7 @@ WSGI_APPLICATION = 'trip_pulse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.{}'.format(
-             os.getenv('DATABASE_ENGINE', 'sqlite3')
-         ),
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER':os.getenv('DB_USER'),
         'PASSWORD':os.getenv('DB_PASSWORD'),
